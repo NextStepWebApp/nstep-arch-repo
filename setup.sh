55dcfa3dcdf80057401e75dfa3514fabe992a1a9
Melchizedek Shah <melchizedekshah18@proton.me>
2026-01-01 19:12:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sudo cat >> /etc/pacman.conf << EOF
+
+sudo sh -c "cat >> /etc/pacman.conf <<'EOF'
 
 [nstep-arch-repo]
 SigLevel = Optional TrustAll
 Server = https://github.com/NextStepWebApp/nstep-arch-repo/raw/refs/heads/main/\$arch
-EOF
+EOF"
 
 sudo pacman -Syyu
+
+sudo pacman -S nstep-git
